977a65a0cac018d7fc918d8cf76019d526a9122a
Nadeem <m.nadeem83005@gmail.com>
2024-03-13 09:13:21 +0530

    please fix

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ REST_API_KEY = 'NTczYjgxNDYtN2RjNi00M2I3LWEwOGMtNjc4ZDVlYjMyMDAw'
 ONESIGNAL_API_URL = 'https://onesignal.com/api/v1/notifications'
 message = {
     "app_id": APP_ID,
-    "include_player_ids": ["ca57bb89-1bd7-4b28-8de2-2d205acfa860"],
     "contents": {"en": "Hooray your dynamic bus has been scheduled!"},
     "headings": {"en": "Dynamic Bus Scheduled"}
 }
@@ -72,6 +71,7 @@ async def check_coordinates(new_coord: str):
         return jsonable_encoder({"message": False})
 def send_notification_to_voters(device_ids):
     message["include_player_ids"] = device_ids
+    print(device_ids)
     response = requests.post(ONESIGNAL_API_URL, headers=headers, data=message_json)
     if response.status_code == 200:
         print("Notification sent successfully to voters!")
@@ -126,10 +126,8 @@ def get_db():
 async def view_databases(db: Session = Depends(get_db)):
     votes = db.query(Vote).all()
     buses = db.query(BUS).all()
-
     template = env.get_template("view_db.html")
     rendered_html = template.render(votes=votes, buses=buses)
-
     return HTMLResponse(content=rendered_html)
 
 
--- onesignal.py
+++ onesignal.py
@@ -8,7 +8,7 @@ ONESIGNAL_API_URL = 'https://onesignal.com/api/v1/notifications'
 
 message = {
     "app_id": APP_ID,
-    "include_player_ids": ["ca57bb89-1bd7-4b28-8de2-2d205acfa860"],
+    "include_player_ids": ["feed1993-441b-4af9-b4c7-39365b263ce4","dc0b5027-0c88-44d9-b945-3b567117f8d5","06d73f55-7ccb-48f9-a2fe-d34cb4f1e29c"],
     "contents": {"en": "Hooray your dynamic bus has been scheduled!"},
     "headings": {"en": "Dynamic Bus Scheduled"}
 }
